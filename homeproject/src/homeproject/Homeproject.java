99064d4398ee791567f867bd57ea47f0abbdde2c
emre <emre@imran>
2016-05-05 08:14:57 +0300

    main class

--- homeproject/src/homeproject/Homeproject.java
+++ homeproject/src/homeproject/Homeproject.java
@@ -16,7 +16,7 @@ public class Homeproject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("dene git !!!!!!!!!!!!!!!!");
+        System.out.println("dene git ");
     }
     
 }
