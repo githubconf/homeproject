cc2cbf81f655184169258fe6e900aff9f4e6beb0
emre <emre@imran>
2016-05-06 08:14:07 +0300

    Merge br1 into br2
    Conflicts:
    	homeproject/src/homeproject/Dene.java

--- homeproject/src/homeproject/Dene.java
+++ homeproject/src/homeproject/Dene.java
@@ -12,7 +12,7 @@ package homeproject;
 public class Dene {
     public Dene(){
         System.out.println("br1 update");
-    }
+    
         System.out.println("br1 brnch message");
     
         System.out.println("dene class br2");
