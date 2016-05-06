dbb17e380a357614a8b97c59a2d2fea4e5a2fb5b
emre <emre@imran>
2016-05-06 07:29:46 +0300

    deneme branch kommit

--- homeproject/src/homeproject/Homeproject.java
+++ homeproject/src/homeproject/Homeproject.java
@@ -16,7 +16,7 @@ public class Homeproject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("dene gitmain class updTE ");
+        System.out.println("dene gitmain class deneme branch ");
     }
     
 }
