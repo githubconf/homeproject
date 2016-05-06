889bd149883557ae8ab42b8e56575d63484fc692
emre <emre@imran>
2016-05-06 08:23:51 +0300

    Merge br1 into br2
    Conflicts:
    	homeproject/src/homeproject/Dene.java

---
+++ homeproject/src/homeproject/Br1Class.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homeproject;
+
+/**
+ *
+ * @author emre
+ */
+public class Br1Class {
+    public Br1Class(){
+        
+    }
+}
